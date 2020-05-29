af6cc4a1683eec1a3538a48de73fa6bfa11b82ef
beltransen <beltransen@gmail.com>
2020-05-29 12:47:30 +0200

    Remove tensorflow dependency. Pytorch tensorboard API used instead.

--- utils/logger.py
+++ utils/logger.py
@@ -1,16 +1,15 @@
-import tensorflow as tf
+from torch.utils.tensorboard import SummaryWriter
 
 class Logger(object):
     def __init__(self, log_dir):
         """Create a summary writer logging to log_dir."""
-        self.writer = tf.compat.v1.summary.FileWriter(log_dir)
-        
+        self.writer = SummaryWriter(log_dir)
+
     def scalar_summary(self, tag, value, step):
         """Log a scalar variable."""
-        summary = tf.compat.v1.Summary(value=[tf.compat.v1.Summary.Value(tag=tag, simple_value=value)])
-        self.writer.add_summary(summary, step)
+        self.writer.add_scalar(tag, value, step)
 
     def list_of_scalars_summary(self, tag_value_pairs, step):
         """Log scalar variables."""
-        summary = tf.compat.v1.Summary(value=[tf.compat.v1.Summary.Value(tag=tag, simple_value=value) for tag, value in tag_value_pairs])
-        self.writer.add_summary(summary, step)
+        for tag, value in tag_value_pairs:
+            self.writer.add_scalar(tag, value, step)
